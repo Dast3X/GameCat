a942cc885d80e60dba3556153974ebf777e80325
Dast3X <65315002+Dast3X@users.noreply.github.com>
2023-06-02 14:52:52 +0300

    Update Role.java

--- src/main/java/com/gamecatalog/spring/models/enums/Role.java
+++ src/main/java/com/gamecatalog/spring/models/enums/Role.java
@@ -1,11 +1,12 @@
 package com.gamecatalog.spring.models.enums;
 
-
 import org.springframework.security.core.GrantedAuthority;
 
+// This enum is used to define user roles
 public enum Role implements GrantedAuthority {
     USER, ADMIN;
 
+    // get authority of user role (USER or ADMIN) - used by Spring Security
     @Override
     public String getAuthority() {
         return name();
