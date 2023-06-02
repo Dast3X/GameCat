3b49eeda97cc423988244283799d0bf7d7ccab92
Dast3X <65315002+Dast3X@users.noreply.github.com>
2023-06-02 14:38:27 +0300

    Update CommentRepository.java

--- src/main/java/com/gamecatalog/spring/repos/CommentRepository.java
+++ src/main/java/com/gamecatalog/spring/repos/CommentRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+// This interface is used to perform CRUD operations on Comment objects
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    // find all comments by game id
     List<Comment> findAllByGameId(Long id);
 }
