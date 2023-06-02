ec8e1ad7ac953045a47d1428902682b1469c2fef
Dast3X <65315002+Dast3X@users.noreply.github.com>
2023-06-02 14:35:44 +0300

    Update GameRepository.java

--- src/main/java/com/gamecatalog/spring/repos/GameRepository.java
+++ src/main/java/com/gamecatalog/spring/repos/GameRepository.java
@@ -6,23 +6,37 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+// This interface is used to perform CRUD operations on Game objects
 public interface GameRepository extends JpaRepository<Game, Long> {
+    
+    // find all games with title starting with given string and having given post date between given dates
     List<Game> findByTitleStartingWithIgnoreCaseAndPostDateBetween(
             String title,
             LocalDateTime startTime,
             LocalDateTime endTime);
 
+    // find all games with title starting with given string and having given trailer url 
     List<Game> findByTitleStartingWithIgnoreCaseAndTrailerUrlIsNotNull(String title);
 
+    // find all games with title starting with given string and not having given trailer url
     List<Game> findByTitleStartingWithIgnoreCaseAndTrailerUrlIsNull(String title);
 
+    // find all games with title starting with given string
     List<Game> findByTitleStartingWithIgnoreCase(String filter);
 
+    /*   
+        find all games with title starting with given string and having given post date between given dates and having 
+        given trailer url
+    */
     List<Game> findByTitleStartingWithIgnoreCaseAndTrailerUrlIsNotNullAndPostDateBetween(
             String title,
             LocalDateTime startTime,
             LocalDateTime endTime);
 
+    /*    
+        find all games with title starting with given string and having given post date between given dates and not 
+        having given trailer url
+    */
     List<Game> findByTitleStartingWithIgnoreCaseAndTrailerUrlIsNullAndPostDateBetween(
             String title,
             LocalDateTime startTime,
