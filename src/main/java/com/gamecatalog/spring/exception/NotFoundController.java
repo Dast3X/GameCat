e5929cd618561830130f42a4a8e05cecdc398311
Dast3X <65315002+Dast3X@users.noreply.github.com>
2023-06-02 14:53:41 +0300

    Update NotFoundController.java

--- src/main/java/com/gamecatalog/spring/exception/NotFoundController.java
+++ src/main/java/com/gamecatalog/spring/exception/NotFoundController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+// This controller is used to redirect to main page when user tries to access non-existing page
 @Controller
 public class NotFoundController {
 
+    // redirect to main page
     @RequestMapping("/**")
     public RedirectView redirectToMainPage() {
         return new RedirectView("/");
