9febb77ac1bf35633bcde49e387e4d52a5517b1b
Dast3X <65315002+Dast3X@users.noreply.github.com>
2023-06-02 14:26:20 +0300

    Update Application.java
    comments

--- src/main/java/com/gamecatalog/spring/Application.java
+++ src/main/java/com/gamecatalog/spring/Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+    
+    /* Run Spring Boot application */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
