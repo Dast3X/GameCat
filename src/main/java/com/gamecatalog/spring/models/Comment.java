f602f0c20ed2fd186964ed351165b022c4159247
Dast3X <65315002+Dast3X@users.noreply.github.com>
2023-06-02 14:52:12 +0300

    Update Comment.java

--- src/main/java/com/gamecatalog/spring/models/Comment.java
+++ src/main/java/com/gamecatalog/spring/models/Comment.java
@@ -2,13 +2,12 @@ package com.gamecatalog.spring.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+// This class is used to represent Comment objects
 @Data
 @Entity
 @NoArgsConstructor
@@ -18,32 +17,36 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String text;
-
     private LocalDateTime postDate;
 
+    // many comments can be posted on one game
     @ManyToOne
     @JoinColumn(name = "game_id")
     private Game game;
 
+    // many comments can be posted by one user
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
+    // set post date before persisting comment
     @PrePersist
     private void init() {
         postDate = LocalDateTime.now();
     }
 
+    // format post date
     public String formatDate(String format) {
         return postDate.format(DateTimeFormatter.ofPattern(format));
     }
 
+    // get author username
     public String getAuthor() {
         return user.getUsername();
     }
 
+    // get author avatar
     public String getAuthorAvatar() {
         return user.getAvatar();
     }
