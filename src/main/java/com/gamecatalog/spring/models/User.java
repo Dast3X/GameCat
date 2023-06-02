18e21262d238df9ac1807d8cde1204ae2505cbb1
Dast3X <65315002+Dast3X@users.noreply.github.com>
2023-06-02 14:49:55 +0300

    Update User.java

--- src/main/java/com/gamecatalog/spring/models/User.java
+++ src/main/java/com/gamecatalog/spring/models/User.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Set;
 
 
+// This class is used to represent User objects
 @Data
 @Entity
 @NoArgsConstructor
@@ -34,61 +35,69 @@ public class User implements UserDetails {
     private boolean active;
 
     private String avatar;
-
+    
     private LocalDateTime registerDate;
-
-
+    
+    // user can have up to 2 roles
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles = new HashSet<>();
 
-
+    // user can have many games
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Game> games;
 
+    // user can have many comments
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Comment> comments;
 
+    // check if user is admin
     public boolean isAdmin() {
         return roles.contains(Role.ADMIN);
     }
-
+    
+    // check if user is active (not banned)
     public boolean isActive() {
         return active;
     }
 
+    // set register date before persisting user
     @PrePersist
     private void init() {
         registerDate = LocalDateTime.now();
     }
 
+    // format date to string
     public String formatDate(String format) {
         return registerDate.format(DateTimeFormatter.ofPattern(format));
     }
 
+    // security - get user roles
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return getRoles();
     }
-
-
-    // security
+    
+    // security - user is not expired
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    // security - user is not locked
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    // security - credentials are not expired
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    // security - user is enabled
     @Override
     public boolean isEnabled() {
         return active;
